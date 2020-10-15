41bf9ff90c898877992cfa2542177eb25a0fcc9d
George <gkasparyants@gmail.com>
2020-10-15 15:22:16 +0300

    macOS stash

--- codes/base.py
+++ codes/base.py
@@ -5,8 +5,12 @@ import bitarray
 
 
 class BaseCode(object):
-    def __init__(self):
+    block_size: int
+
+    def __init__(self, block_size: int = 7, code_size: int = 3):
         super(BaseCode, self).__init__()
+        self.block_size = block_size
+        self.code_size = code_size
 
     def encode(self, array: np.ndarray) -> np.ndarray:
         raise NotImplementedError()
@@ -18,17 +22,15 @@ class BaseCode(object):
     def is_ascii(s):
         return all(ord(c) < 128 for c in s)
 
-    @staticmethod
-    def string2bit(s: str) -> np.ndarray:
+    def string2bit(self, s: str) -> np.ndarray:
         assert BaseCode.is_ascii(s)
-        x = bitarray.bitarray(''.join([bin(ord(x))[2:].zfill(8) for x in s]))
+        x = bitarray.bitarray(''.join([bin(ord(x))[2:].zfill(self.block_size) for x in s]))
         return np.array(x.tolist(), dtype=np.int32)
 
-    @staticmethod
-    def bit2string(x: np.ndarray) -> str:
-        assert x.shape[0] % 8 == 0
+    def bit2string(self, x: np.ndarray) -> str:
+        assert x.shape[0] % self.block_size == 0
         x = ''.join(map(str, (x % 2).tolist()))
-        x = ''.join([chr(int(x[i: i + 8], 2)) for i in range(0, len(x), 8)])
+        x = ''.join([chr(int(x[i: i + self.block_size], 2)) for i in range(0, len(x), self.block_size)])
         return x
 
     @staticmethod
@@ -38,3 +40,7 @@ class BaseCode(object):
     @staticmethod
     def flip(x: np.ndarray) -> np.ndarray:
         return (x + 1) % 2
+
+    @staticmethod
+    def hamming(a, b):
+        return np.sum((a + b) % 2)
